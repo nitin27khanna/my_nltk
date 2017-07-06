38621ab0b950e0fc129a677928b060f48b2cfaee
nitin27khanna <nitin007veer@gmail.com>
2017-07-06 09:27:34 +0530

    Create Check_semtiment_mod.py

---
+++ Check_semtiment_mod.py
@@ -0,0 +1,6 @@
+import sentiment_mode as s
+
+print(s.sentiment("This movie was awesome ,plot was wonderful and pythoned"))
+
+print(s.sentiment("This movie was utter junk ,plot was shit and absolutely shitty"))
+
